86897ec3746ecf9a855ed2fed73d3381232a2d23
anshu <anshu@pop-os.localdomain>
2022-04-24 12:58:25 +0530

    mineBlock add

--- block.js
+++ block.js
@@ -1,8 +1,30 @@
-class Block{
-	constructor(timestamp, lastHash, hash, data) {
-		this.timestamp = timestamp;
-		this.lastHash = lastHash;
-		this.hash = hash;
-		this.data = data;
+class Block {
+	constructor({ timestamp, lastHash, hash, data, nonce, difficulty }) {
+	  this.timestamp = timestamp;
+	  this.lastHash = lastHash;
+	  this.hash = hash;
+	  this.data = data;
+	  this.nonce = nonce;
+	  this.difficulty = difficulty;
+	}
+  
+	static genesis() {
+	  return new this(GENESIS_DATA);
+	}
+
+	static mineBlock({ lastBlock, data }) {
+		const lastHash = lastBlock.hash;
+		let hash, timestamp;
+		let { difficulty } = lastBlock;
+		let nonce = 0;
+	
+		do {
+		  nonce++;
+		  timestamp = Date.now();
+		  difficulty = Block.adjustDifficulty({ originalBlock: lastBlock, timestamp });
+		  hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
+		} while (hexToBinary(hash).substring(0, difficulty) !== '0'.repeat(difficulty));
+	
+		return new this({ timestamp, lastHash, data, difficulty, nonce, hash });
 	}
 }
